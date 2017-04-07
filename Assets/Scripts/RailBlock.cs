c618b02bda6daf1f4a89b29d0ce4c69950466d0f
Unknown <jak3matt@gmail.com>
2017-04-06 18:54:55 -0700

    Project Transfer
    Project start

---
+++ Assets/Scripts/RailBlock.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RailBlock : MonoBehaviour
+{
+    [SerializeField] private Transform lane;
+    [SerializeField] private float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+	}
+
+    public void SetScale(int minLength, int maxLength) {
+        transform.parent.localScale = new Vector3(1.0f, 1.0f, Random.Range(minLength, maxLength));
+    }
+
+    public void SetLane(Transform t)
+    {
+        lane = t;
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other.name == "DestroyZone") {
+            gameObject.SetActive(false);
+        }
+        else if (other.name == "SpawnZone") {
+            WorldSpawner.Instance.SpawnRail(lane);
+        }
+    }
+}
---
+++ Assets/Scripts/WorldSpawner.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldSpawner : MonoBehaviour
+{
+    private static WorldSpawner instance;
+    [SerializeField] private GameObject railBlock;
+    [SerializeField] private Transform[] lanePoints;
+    [SerializeField] private Transform spawnZone;
+    [SerializeField] private Transform destroyZone;
+    [SerializeField] private int maxHeight = 0;
+    [SerializeField] private int minHeight = 0;
+    [SerializeField] private float spawnRate = 0.0f;
+
+    public static WorldSpawner Instance { get { return instance; } set { instance = value; } }
+
+    // Use this for initialization
+    void Awake () {
+        if (instance == null) {
+            instance = this;
+        }
+    }
+
+    void Start () {
+        //SpawnRails();
+	}
+	
+	// Update is called once per frame
+	void Update () {   
+	
+	}
+
+    public void SpawnRails() {
+        for (int i = 0; i < lanePoints.Length; i++)
+        {
+            Instantiate(railBlock, new Vector3(lanePoints[i].position.x, minHeight, lanePoints[i].position.z), lanePoints[i].rotation);
+        }
+    }
+
+    public void SpawnRail(Transform t) {
+        GameObject gO = (GameObject) Instantiate(railBlock, new Vector3(t.position.x, t.position.y, t.position.z + 2.0f), t.rotation);
+        RailBlock rB = gO.GetComponent<RailBlock>();
+        rB.SetScale(2, 31);
+        rB.SetLane(t);
+    }
+}
